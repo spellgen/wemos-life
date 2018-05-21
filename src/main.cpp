fec1f0023d1c8b18e2cc97c79f2d33de92afe6b8
Stefan Engstrom <sengstrom@ena.com>
2018-05-21 18:13:12 -0500

    works now

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 #include <WEMOS_Matrix_LED.h>
 
-MLED mled(5); //set intensity=5
+MLED mled(1); //set intensity=5
 
 uint8 state1[8] = {0x00, 0x00, 0x1c, 0x10, 0x08, 0x00, 0x0e, 0x00};
-uint8 state2[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x04, 0x02};
-uint8 state3[8] = {0x00, 0x00, 0x00, 0x00, 0xe2, 0x03, 0x40, 0x00};
+uint8 state2[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x04, 0x02}; // glider
+uint8 state3[8] = {0x00, 0x00, 0x00, 0x00, 0xe2, 0x03, 0x40, 0x00}; // metusaleh
+uint8 state4[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x38, 0x20, 0x10};
 
-uint8 current[8];
+uint8 *current = state4;
 uint8 previous[8];
+
+uint8 count = 0;
 
 int loadState(uint8 *to, uint8 *from) {
     for(int i=0;i<8; i++) {
@@ -24,40 +27,43 @@ int get(uint8 *map, int i, int j) {
   return row & (1<<col);
 }
 
-// set the state of map[i,j] to s
+// set the state of map[i,j] to s - i and j in the range [0,7]
 void set(uint8 *map, int i, int j, int s) {
-  int row = map[i%8];
-  int col = j%8;
-  int mask = (1<<col); // the bit we are going to set
+  int mask = (1<<j); // the bit we are going to set
   if (s) {
-    map[i%8] = row | mask; // set the position to true
+    map[i] |= mask; // set the position to true
   } else {
-    map[i%8] = row & !mask; // clear the position
+    map[i] &= (mask ^ 0xff); // clear the position
   }
+}
+
+void print(int idx, uint8 *map) {
+  Serial.printf("=== iteration %d ===\n",idx);
+  for (int i=0; i<8; i++) {
+    for (int j=0; j<8; j++) {
+      Serial.printf("%s",(get(map,7-i,j)? "x": "."));
+    }
+    Serial.printf("\n");
+  }
+  Serial.printf("\n");
 }
 
 void setup() {
   Serial.begin(115200);
-  // delay(4000);
   Serial.println("Starting");
 
-  loadState(current, state3);
-  // for(int i=1; i<8; i++)  {
-  //   mled.disBuffer[i]=current[i];  //full screen
-  // }
+  // loadState(current, state1);
 
-  // set(current,0,4,1);
-  // Serial.printf("A: [0,4]=%d\n",get(current,0,4));
-  // Serial.printf("A: [8,4]=%d\n",get(current,8,4));
-  // set(current,0,4,0);
-  // Serial.printf("B: [0,4]=%d\n",get(current,0,4));
-  // set(current,0,4,1);
-  // Serial.printf("C: [0,4]=%d\n",get(current,0,4));
+  // initial load current into the display
+  for(int i=0; i<8; i++)  {
+    mled.disBuffer[i]=current[i];  //full screen
+  }
 
-  // mled.clear();
+  mled.display();
+  delay(2000); // display the initial state
+  mled.intensity = 5;
+  print(count, current);
 }
-
-uint8 count = 1;
 
 void loop() {
 
@@ -70,21 +76,13 @@ void loop() {
   //   exit(0);
   // }
 
-  // load current into the display
-  for(int i=0; i<8; i++)  {
-    mled.disBuffer[i]=current[i];  //full screen
-  }
-  mled.display();
-
-  delay(500);
-
   // copy current into previous
   loadState(previous,current);
 
-  for(int i=0;i<8;i++){
-     for(int j=0;j<8;j++) {
+  for(int i=0; i<8; i++){
+     for(int j=0; j<8; j++) {
 
-      int p = get(previous, i,j);
+      int p = get(previous, i, j);
       int alive = 0;
 
       for (int k=-1; k<=1; k++) {
@@ -94,7 +92,7 @@ void loop() {
             continue; // don't count ourselves
           }
 
-          if (get(previous,i+k,j+l)) {
+          if (get(previous, i+k, j+l)) {
             alive++; // count alive neighbors
           }
         }
@@ -106,26 +104,35 @@ void loop() {
         switch (alive) {
           case 0:
           case 1:
-            set(current,i,j,0); // underpopulation
-            Serial.printf("[%d,%d] dies from underpopulation\n",i,j);
+            set(current, i, j, 0); // underpopulation
+            Serial.printf("[%d,%d] dies from underpopulation (%d)\n",i,j, alive);
             break;
           case 2:
           case 3:
-            set(current,i,j,1); // just right, live on
-            Serial.printf("[%d,%d] keeps on living\n", i,j);
+            set(current, i, j, 1); // just right, live on
+            Serial.printf("[%d,%d] keeps on living (%d)\n", i,j,alive);
             break;
           default:
-            set(current,i,j,0); // overpopulation
-            Serial.printf("[%d,%d] dies from overpopulation\n",i,j);
+            set(current, i, j, 0); // overpopulation
+            Serial.printf("[%d,%d] dies from overpopulation (%d)\n",i,j, alive);
         }
       } else { // the current cell is dead
         if (alive==3) {
-          set(current, i,j,1); // reproduction creates new cell
-          Serial.printf("[%d,%d] created\n",i,j);
+          set(current, i, j, 1); // reproduction creates new cell
+          Serial.printf("[%d,%d] created (%d)\n",i,j, alive);
         }
       }
 
     }
   }
+
+  print(count,current);
+
+  // load current into the display
+  for(int i=0; i<8; i++)  {
+    mled.disBuffer[i]=current[i];  //full screen
+  }
+  mled.display();
+  delay(300);
 }
 
